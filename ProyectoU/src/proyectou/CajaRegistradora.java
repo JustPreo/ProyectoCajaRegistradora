ec32821fa1a7872d166324c12bfa6ed36b6a05af
JustPreo <impre0hn@gmail.com>
2025-04-27 12:17:23 -0600

    Inicio de proyecto

---
+++ ProyectoU/src/proyectou/CajaRegistradora.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectou;
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ */
+public class CajaRegistradora {
+    public static void main(String[]args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        //Menu Inicial
+        
+        System.out.println("***MENU***"+
+                "\nHola");
+        
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
