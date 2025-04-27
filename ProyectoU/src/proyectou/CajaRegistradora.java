0170ceac9ed519579428b7efd6bc0b7036246d6e
JustPreo <impre0hn@gmail.com>
2025-04-27 15:35:48 -0600

    Base Inicial
     Basicamente implemente lo que seria el mismatch error , el scanner basico y los switch basicos.
    Elige la opcion y devuelve al menu.
    Por el momento esta es la base , despues se editara para agregar abrir caja.

--- ProyectoU/src/proyectou/CajaRegistradora.java
+++ ProyectoU/src/proyectou/CajaRegistradora.java
@@ -3,8 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyectou;
-import java.util.Scanner;
-
+import java.util.Scanner; //Import Scanner
+import java.util.InputMismatchException; //Import MismatchException
 /**
  *
  * @author user
@@ -13,19 +13,52 @@ public class CajaRegistradora {
     public static void main(String[]args)
     {
         Scanner input = new Scanner(System.in);
-        
+        boolean Seguir = true;
+        int Opcion = 0;
         //Menu Inicial
-        
+        while (Seguir == true){
         System.out.println("***MENU***"+
-                "\nHola");
+                "\n1.Abrir Caja"+
+                "\n2.Ventas"+
+                "\n3.Compras"+
+                "\n4.Cerrar Caja"+
+                "\n5.Salir del programa");
+        System.out.println("Porfavor ingrese la opcion a elegir");
+        try { //Misma logica que el try de python
+        Opcion = input.nextInt();
+        }
+        catch (InputMismatchException e) //Esto es necesario es como el try ; except de python
+        {
+        System.out.println("Error: Debe de ingresar un numero entero");
+        input.next();
+        continue; //Volver al bucle y que no se repita eso de ingrese uno correcto
         
+        }
+        switch(Opcion)
+        {
+            case 1:
+                System.out.println("Abrir Caja");
+                break;
+            case 2:
+                System.out.println("Ventas");
+                break;
+            case 3:
+                System.out.println("Compras");
+                break;
+            case 4:
+                System.out.println("Cerrar Caja");
+                break;
+            case 5:
+                System.out.println("Salir del programa");
+                break;
+            default:
+                System.out.println("Elige una opcion valida");
+                break;
+        
+        }//Switch
+        }//Seguir
+
     
-    
-    
-    
-    
-    
-    
-    }
+    }//Static void
     
 }
